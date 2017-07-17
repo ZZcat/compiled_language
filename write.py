d9bd6a09d55691af8715d99368cb5b713f0d3f95
Spincat360 <stea@Zachs-Mac.local>
2017-07-17 15:02:35 -0500

    Add open

--- write.py
+++ write.py
@@ -2,15 +2,30 @@ from Tkinter import *
 
 def save():
     data = text_area.get("1.0",END)
-    result.set('Lines: '+str(len(data.split('\n'))-1))
-    print data,
     filename = data.split('\n', 1)[0]
     filename = filename.split(" ", 1)[1]
     print filename
-def open():
-    print "Open"
+    with open(filename, "w") as file:
+        for line in data:
+            file.write(line)
+        file.close()
+    result.set('Lines: '+str(len(data.split('\n'))-1)+" Filename: " + filename)
+
+def fileopen():
+    data = text_area.get("1.0",END)
+    filename = data.split('\n', 1)[0]
+    filename = filename.split(" ", 1)[1]
+    file = open(filename, "r") 
+    text_area.delete(1.0, END)
+    text_area.insert(END, file.read())
+    data = text_area.get("1.0",END)
+    filename = data.split('\n', 1)[0]
+    filename = filename.split(" ", 1)[1]
+    result.set('Lines: '+str(len(data.split('\n'))-1)+" Filename: " + filename)
+
     
 window = Tk()
+window.wm_title("Program editor")
 
 frame=Frame(window)
 frame.pack()
@@ -23,7 +38,7 @@ result.set('')
 label=Label(window,textvariable=result)
 label.pack()
 
-button=Button(window,text="Open", command=open)
+button=Button(window,text="Open", command=fileopen)
 button.pack()
 button=Button(window,text="Save", command=save)
 button.pack()
