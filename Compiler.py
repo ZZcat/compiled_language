1b8a6acd74b590ebf9e1ab02e36a13a45c2e40ee
Spincat360 <stea@Zachs-Mac.local>
2017-07-17 19:57:53 -0500

    Bug fix

--- Compiler.py
+++ Compiler.py
@@ -11,9 +11,8 @@ for line in data.split('\n'):
     if line_number > 1:
         line_parts = line.split(' ')
         command,values = line_parts
-        if command.lower() == "set":
-            print line_parts
-            print values
+        if 1 == 0:
+            pass
         else:
             for value in values.split(','):
                 #print value
@@ -30,8 +29,8 @@ for line in data.split('\n'):
                         # "Command"
 constants = list(set(constants))
 variables = list(set(variables))
-memery = variables
-print memery
+memery = constants
+memery.extend(variables)
 with open(filename_start+"_mem.txt", "w") as file:
     for memery_part in memery:
         file.write(memery_part+"\n")
@@ -45,51 +44,61 @@ for line in data.split('\n'):
     if line_number > 1:
         line_parts = line.split(' ')
         command,values = line_parts
-        if command.lower() == "set":
-            print line_parts
+        if command.lower() == "setv":
+            file = open(filename_start+"_mem.txt", "r+") 
+            data = file.read()
+            data_new=""
+            for data_replace in data.split('\n'):
+
+                if data_replace == values[0]:
+                    data_new = data_new + values[2]
+                else:
+                    data_new = data_new + data_replace
+                data_new = data_new + "\n"
+            file.seek(0)
+            file.truncate()
+            file.write(data_new)
+            file.close()
         else:
             program = program + command
             program = program + " "
             for value in values.split(','):
                 try:
                     int(value)+1
-                    program = program + value
+                    program = program + ("{0:b}".format((memery.index(value))))
                 except:
                     if value.isalpha() == True:
-                        program = program + value
-                        #program = program + ("{0:b}".format(memery.index(value)))
+                        program = program + ("{0:b}".format(memery.index(value)))
                     else:
                         program = program + value
                 program = program + ","
             program = program + "\n"
+file = open(filename_start+"_mem.txt", "r+") 
+data = file.read()
+data_new = ""
+for data_line in data.split('\n'):
+    if not data_line == "":
+        try:
+            int(data_line)
+            data_new = data_new + str(int(data_line))
+        except:
+            data_new = data_new + str(0)
+        data_new = data_new + "\n"
+file.seek(0)
+file.truncate()
+file.write(data_new)
+file.close()
+
+
 program_new = ""
 for program_line in program.split('\n'):
     program_new = program_new+program_line[:-1]+"\n"
 
-
-line_number = 1
+line_number = -1
 for line in program_new.split('\n'):
     line_number = line_number+1
-    if line_number > 1:
-        try:
-            line_parts = line.split(' ')
-            command,values = line_parts
-            command = command.lower()
-            if command == "set":
-                print "set",values # Fix in compiler var define
- #               newcontents = contents.replace('a','e')
-                file = open(filename_start+"_mem.txt", "r+") 
-                data = file.read()
-                print "aaaaa"
-                print (values.split(',')[0])
-                print (values.split(',')[1])
-                data = data.replace((values.split(',')[0]),(values.split(',')[1]))
-                file.close()
-        except:
-            pass
-line_number = 1
-for line in program_new.split('\n'):
-    line_number = line_number+1
+    address = "{0:b}".format(line_number)
+    print address.zfill(4)+": ",
     if line_number > 1:
         try:
             line_parts = line.split(' ')
@@ -110,28 +119,39 @@ for line in program_new.split('\n'):
             elif command == "stop":
                 print "0000 0000"
             elif command == "while":
-                pass
+                while_values = values.split(',')
+                while_address = address
+            elif command == "whileend":
+                if while_value[1] == "=":
+                    if while_values[2].isalpha()==True:
+                        pass # var
+                    else:
+                        if while_values[2] == 0:
+                            print "0010 ", while_values[0].zfill(4)
+                            print "1000 ", while_address
+                        else:
+                            pass # int
+                if while_value[1] == "=!":
+                    pass
+                if while_value[1] == ">":
+                    pass
+                if while_value[1] == "<":
+                    pass
+                
             elif command == "forever":
                 pass
             elif command == "if":
                 pass
             elif command == "out":
                 for value in values.split(','):
-                    print "0010 ",
-                    print value.zfill(4)
+                    print "0010 ",value.zfill(4)
                 print "0111 0000"
             elif command == "clear":
                 print "0111 0011"
-            elif command == "set--":
-                print "set",values # Fix in compiler var define
- #               newcontents = contents.replace('a','e')
-                file = open(filename_start+"_mem.txt", "r+") 
-                data = file.read()
-                print "aaaaa"
-                print (values.split(',')[0])
-                print (values.split(',')[1])
-                data = data.replace((values.split(',')[0]),(values.split(',')[1]))
-                file.close()
+            elif command == "set":
+                pass
+            else:
+                print "----UNKNOWN----"
         except:
             pass #empty line
         
