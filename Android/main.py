94223fec27f4e634d2ef2fd22587fbb9b3931753
Spincat360 <stea@Zachs-Mac.local>
2018-02-03 09:46:28 -0600

    More buttons

--- Android/main.py
+++ Android/main.py
@@ -19,16 +19,32 @@ def press_callback(obj):
         if obj.state == "down":
             address = ('192.168.253.101', 6001)
             global conn
+            global conn_open
+            conn_open = 1
             conn = Client(address, authkey='iLikeFatCats! bc8719873v4yb9c8yc8n2b98c927b618v7c8')
             global conn
-            conn.send('test')
-            conn.send('test2')
+            global conn_open
+            conn.send('open')
         else:
-            pass
+            conn_open = 0
             conn.send('close')
             conn.close()
-
-
+    elif obj.text == 'Blue':
+        if obj.state == "down":
+            pass
+        else:
+            pass
+    if 1:
+        try:
+            if conn_open == 1:
+                global conn_open
+                global conn
+                if obj.state == "down":
+                    conn.send(obj.text+str(0))
+                else:
+                    conn.send(obj.text+str(1))
+        except:
+            pass
 
 
 # This is called when the slider is updated:
@@ -49,29 +65,61 @@ class MyApp(App):
         connB = ToggleButton(text="Open connection")
         connB.bind(on_press=press_callback)
         
-        a = ToggleButton(text="0")
-        a.bind(on_press=press_callback)
-        b = ToggleButton(text="1")
-        b.bind(on_press=press_callback)
-        c = ToggleButton(text="2")
-        c.bind(on_press=press_callback)
-        d = ToggleButton(text="3")
-        d.bind(on_press=press_callback)
-        e = ToggleButton(text="4")
-        e.bind(on_press=press_callback)
-        f = ToggleButton(text="5")
-        f.bind(on_press=press_callback)
-        g = ToggleButton(text="6")
-        g.bind(on_press=press_callback)
-        h = ToggleButton(text="7")
-        h.bind(on_press=press_callback)
         
+        a = ToggleButton(text="D0")
+        a.bind(on_press=press_callback)
+        b = ToggleButton(text="D1")
+        b.bind(on_press=press_callback)
+        c = ToggleButton(text="D2")
+        c.bind(on_press=press_callback)
+        d = ToggleButton(text="D3")
+        d.bind(on_press=press_callback)
+        e = ToggleButton(text="D4")
+        e.bind(on_press=press_callback)
+        f = ToggleButton(text="D5")
+        f.bind(on_press=press_callback)
+        g = ToggleButton(text="D6")
+        g.bind(on_press=press_callback)
+        h = ToggleButton(text="D7")
+        h.bind(on_press=press_callback)
+        i = ToggleButton(text="A0")
+        i.bind(on_press=press_callback)
+        j = ToggleButton(text="A1")
+        j.bind(on_press=press_callback)
+        k = ToggleButton(text="A2")
+        k.bind(on_press=press_callback)
+        l = ToggleButton(text="A3")
+        l.bind(on_press=press_callback)
+        m = ToggleButton(text="A4")
+        m.bind(on_press=press_callback)
+        n = ToggleButton(text="A5")
+        n.bind(on_press=press_callback)
+        o = ToggleButton(text="A6")
+        o.bind(on_press=press_callback)
+        p = ToggleButton(text="A7")
+        p.bind(on_press=press_callback)
+        q = ToggleButton(text="CE")
+        q.bind(on_press=press_callback)
+        r = ToggleButton(text="OW")
+        r.bind(on_press=press_callback)
+        s = ToggleButton(text="WE")
+        s.bind(on_press=press_callback)
+        ard = ToggleButton(text="Blue")
+        ard.bind(on_press=press_callback)
+##        u = ToggleButton(text="D4")
+##        u.bind(on_press=press_callback)
+##        v = ToggleButton(text="D5")
+##        v.bind(on_press=press_callback)
+##        w = ToggleButton(text="D6")
+##        w.bind(on_press=press_callback)
+##        x = ToggleButton(text="D7")
+##        x.bind(on_press=press_callback)
         
     
 #        timeSlider = Slider(orientation='vertical', min=1, max=30, value=speed)
 #        timeSlider.bind(on_touch_down=update_speed, on_touch_move=update_speed)
 
-        buttons = [connB,a,b,c,d,e,f,g,h]
+        buttons = [connB,ard,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s]
         for b in buttons:
             layout.add_widget(b)
         
